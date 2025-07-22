fbf61b390601b26ae2c28d2a038cfbf91f541224
Danny Belloli <danny@practicas.mnxonline.com>
2025-07-22 12:32:35 +0100

    todo funciona bien

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { AppService } from './app.service';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
+
+
